2e7106af67d601cc379aa83c3b4e51e67d947105
Masao KATO <ponkore@gmail.com>
2017-09-15 09:37:42 +0900

    initial commit

---
+++ MouseMover/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MouseMover
+{
+    class Program
+    {
+        private static int IDLE_WAIT = 1000 * 60 * 3; // 3 minutes
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // SetCursorPos を使うのは誤り。これでは ScreenSaver が認識してくれない。
+                // jdk7 では mouse_event(MOUSEEVENTF_MOVE, x, y, 0, 0) を使っていた。
+                //   https://msdn.microsoft.com/ja-jp/library/cc410921.aspx
+                //   但し、ここによると mouse_move ではなく SendInput を使え、とのこと。
+                Win32.mouse_event(Win32.MOUSEEVENTF_MOVE, 1, 1, 0, 0);
+                System.Threading.Thread.Sleep(10);
+                Win32.mouse_event(Win32.MOUSEEVENTF_MOVE, -1, -1, 0, 0);
+                System.Threading.Thread.Sleep(IDLE_WAIT);
+            }
+        }
+    }
+}
---
+++ MouseMover/Win32.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MouseMover
+{
+    public class Win32
+    {
+        // https://stackoverflow.com/questions/8050825/how-to-move-mouse-cursor-using-c
+        // こっちは使わないことにしたが定義を残しておく
+        [DllImport("User32.Dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern long SetCursorPos(int x, int y);
+
+        // こっちを使う。
+        public const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        public const int MOUSEEVENTF_MOVE       = 0x0001;
+        public const int MOUSEEVENTF_LEFTDOWN   = 0x0002;
+        public const int MOUSEEVENTF_LEFTUP     = 0x0004;
+        public const int MOUSEEVENTF_RIGHTDOWN  = 0x0008;
+        public const int MOUSEEVENTF_RIGHTUP    = 0x0010;
+        public const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        public const int MOUSEEVENTF_MIDDLEUP   = 0x0040;
+
+        [DllImport("USER32.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+    }
+}
